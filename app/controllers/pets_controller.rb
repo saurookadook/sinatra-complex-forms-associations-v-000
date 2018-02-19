9c6ce4dca96641c69c3e454055db05a9f46265f7
Andy Maskiell <maskiella@gmail.com>
2018-02-19 20:36:28 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,12 +12,11 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
-    if
-    elsif !params["pet"]["owner_id"].empty?
-      @pet.owner_id = params[:pet][:owner_id]
-    elsif !params["owner_name"].empty?
+    if !params["owner_name"].empty?
       owner = Owner.find(name: params[:owner_name])
       @pet.owner_id = owner.id
+    elsif !params["pet"]["owner_id"].empty?
+      @pet.owner_id = params[:pet][:owner_id]
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
