45c5e3469800954930fe88c21442761ece6b59e0
Andy Maskiell <maskiella@gmail.com>
2018-02-19 20:47:11 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(params[:pet][:name])
+    @pet = Pet.create(name: params[:pet][:name])
     if !params[:pet][:owner_id].empty?
       @pet.owner_id = params[:pet][:owner_id]
     elsif !params[:owner_name].empty?
