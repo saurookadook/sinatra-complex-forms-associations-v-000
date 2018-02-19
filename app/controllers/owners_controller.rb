57394a2a4476d13c3f3b8f364c13eabd3b90a269
Andy Maskiell <maskiella@gmail.com>
2018-02-19 20:28:36 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -12,7 +12,7 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     @owner = Owner.create(params[:pet])
-    if !params["pet"]["name"].empty?
+    if !params["owner"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     @owner.save
